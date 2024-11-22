e063f52665eea68f2efbc31c5ac6d4686c8f62fe
Jani <jani.kaljunen@gmail.com>
2024-11-22 08:21:53 +0200

    ignore_long_expired=True to backup expired contracts

--- sysproduction/backup_db_to_csv.py
+++ sysproduction/backup_db_to_csv.py
@@ -46,7 +46,8 @@ def backup_db_to_csv():
 # SHOULD BE A 'BACKUP X' OPTION UNDER DIAGNOSTICS OR CONTROL?
 def quick_backup_of_all_price_data_including_expired():
     backup_data = get_data_and_create_csv_directories("Quick backup of all price data")
-    backup_futures_contract_prices_to_csv(backup_data, ignore_long_expired=False)
+    # backup also expired contracts
+    backup_futures_contract_prices_to_csv(backup_data, ignore_long_expired=True)
 
 
 class backupDbToCsv:
