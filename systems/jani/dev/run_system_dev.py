08094db9ab07510f3ec9ca00c148a7420540c199
Jani <jani.kaljunen@gmail.com>
2025-08-17 20:28:00 +0300

    new instruments

--- systems/jani/dev/run_system_dev.py
+++ systems/jani/dev/run_system_dev.py
@@ -1,7 +1,7 @@
 #
 # development system backtest
 #
-DEFAULT_CONFIG = "systems.jani.dev.test_prod_80B_500k_debug_equalw.yaml"
+DEFAULT_CONFIG = "systems.jani.dev.test_prod_80B_500k_v2.yaml"
 
 import sys
 import datetime
